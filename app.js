8b921286576d4fc6c5f518476434bc343969e331
Ewan <ewanmillar97@gmail.com>
2026-06-25 02:10:42 +0100

    test 1

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-function searchPokemon() {
-  alert("Button clicked!");
+async function searchPokemon() {
+  const name = document.getElementById("search").value.toLowerCase();
+
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  const data = await res.json();
+
+  document.getElementById("result").innerHTML = `
+    <h2>${data.name}</h2>
+    <p>Height: ${data.height}</p>
+    <p>Weight: ${data.weight}</p>
+    <p>Types: ${data.types.map(t => t.type.name).join(", ")}</p>
+  `;
 }
